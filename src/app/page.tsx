9b25918d99e92ee1db200ba88e3b243434cf299a
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-02 14:38:07 -0700

    Keep sign up within our app, redirect back to homepage after successful sign up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import { SignedOut } from "@clerk/nextjs";
-import Head from "next/head";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function Home() {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-500 to-purple-500">
-            <Head>
-                <title>Hello App Router with Next.js & Clerk</title>
-                <meta
-                    name="description"
-                    content="A simple Hello World homepage using Next.js and TailwindCSS"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
             <div className="bg-white p-8 rounded-xl shadow-lg">
                 <h1 className="text-2xl font-bold mb-4 text-blue-500">Hello, App Router!</h1>
                 <p className="text-gray-600">
@@ -29,6 +19,11 @@ export default function Home() {
                         </div>
                     </Link>
                 </SignedOut>
+                <SignedIn>
+                    <div>
+                        <UserButton afterSignOutUrl="/" />
+                    </div>
+                </SignedIn>
             </div>
         </div>
     );
