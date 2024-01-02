2262524ec9d31e88c3b8f54d8a6bd16a15712ba0
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-02 14:45:45 -0700

    Check for authentication on the client, rendering the component on the client

---
+++ src/app/protected-client/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+
+export default function ClientSideRenderExample() {
+    const { isLoaded, isSignedIn, user } = useUser();
+
+    if (!isLoaded || !isSignedIn) {
+        return null;
+    }
+
+    return (
+        <div>
+            <h1>Welcome, {user.firstName}!</h1>
+            <p>This is your protected page on the client.</p>
+            {/* Other user-specific JSX components/data can be added here */}
+        </div>
+    );
+}
